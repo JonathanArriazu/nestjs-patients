7640b2793c5dfb9a12713ff2c4562305871b71db
Jonathan Arriazu <joni.arriazu2@gmail.com>
2024-04-26 12:26:03 -0300

    Feat: script created to generate modules

---
+++ generate-module.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MODULE_NAME=$1
+
+# Generar el módulo
+nest generate module modules/$MODULE_NAME
+
+# Generar el servicio dentro del módulo
+nest generate service modules/$MODULE_NAME
+
+# Generar el controlador dentro del módulo
+nest generate controller modules/$MODULE_NAME
