79d8c93eca595d55774f2adcf4a6582a75a3c2bb
Jonathan Arriazu <joni.arriazu2@gmail.com>
2024-04-30 11:26:07 -0300

    fix: PORT from .env added to app.listen in main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
+import { ConfigService } from '@nestjs/config';
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+  //const PORT = app.get(ConfigService).get<number>('PORT');
+  const PORT = configService.get<number>('PORT');
+
+  await app.listen(PORT);
 }
 bootstrap();
