d5b00ab8bbe6233859eb531a161e327f1b913c95
Jonathan Arriazu <joni.arriazu2@gmail.com>
2024-04-30 10:05:23 -0300

    feat: some error messages added

--- src/modules/patients/patients.service.ts
+++ src/modules/patients/patients.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, UpdateResult } from 'typeorm';
 import { Patient } from './entities/patients.entity';
@@ -14,9 +14,15 @@ export class PatientsService {
     return this.patientsRepository.find();
   }
 
-  async createPatient(firstName: string, lastName: string, isActive: boolean): Promise<Patient> {
-    const newPatient = this.patientsRepository.create({ firstName, lastName, isActive });
-    return await this.patientsRepository.save(newPatient);
+  async createPatient(firstName: string, isActive: boolean): Promise<Patient> {
+    const newPatient = this.patientsRepository.create({ firstName, isActive });
+
+    try {
+      const savedPatient = await this.patientsRepository.save(newPatient);
+      return savedPatient;
+    } catch (error) {
+      throw new HttpException('Failed to create patient', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   findOne(id: number): Promise<Patient | null> {
@@ -36,17 +42,26 @@ export class PatientsService {
   } */
   async removePatient(id: number): Promise<void> {
     const existingPatient = await this.patientsRepository.findOne({
-      where: { id }, // Buscar paciente por ID
+      where: { id }
     });
 
-    if (!existingPatient) {
-      throw new NotFoundException(`Patient with ID ${id} not found`);
-    }
-
-    // Cambiar isActive a false
     existingPatient.isActive = false;
 
-    // Guardar los cambios en la base de datos
     await this.patientsRepository.save(existingPatient);
   }
+
+  /* async remove(idcategory: string): Promise<DeleteResult | undefined> {
+    try {
+      const result = await this.categoryRepository.softDelete(idcategory);
+      if (result.affected === 0) {
+        throw new HttpException('Order not found', HttpStatus.NOT_FOUND);
+      }
+      return result;
+    } catch (error) {
+      throw new HttpException(
+        'An error occurred while deleting the category',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  } */
 }
